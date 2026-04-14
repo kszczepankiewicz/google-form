262730a7b8b71b6774bd86331b2d434c699b7dcc
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-14 06:25:20 +0200

    feat(index.html): add form submitting

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const url = `https://www.google.com/search`;
+//     const text = `Hint without solution:\n`;
+//     window.open(`${url}q=${text}${input.value}`)
+// })
