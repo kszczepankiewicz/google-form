901fe776f05a38899dbce10e4ddd7de8ef8a3e0a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-14 07:25:02 +0200

    feat(form listener): add opening query

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const url = `https://www.google.com/search`;
-//     const text = `Hint without solution:\n`;
-//     window.open(`${url}q=${text}${input.value}`)
-// })
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const url = `https://www.google.com/search?`;
+    const text = `Hint without solution: \n`;
+    const query = `${url}q=${text}${input.value}`;
+    window.open(query)
+})
+
