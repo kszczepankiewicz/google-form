423335721dd53138c1832966697bc1903f0f3da8
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-14 11:46:06 +0200

    feat: add history recording

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const hintForm = document.getElementById('hint-form');
 const hintInput = document.getElementById('hint-input');
+const history = document.getElementById('history');
+const hintLabel = document.getElementById('hint-label');
 
-hintForm.addEventListener('submit', (e) => {
-    google(e);
-})
+const googleUrl = `https://www.google.com/search?q=`;
 
-function google(e) {
+const addToHistory = (prompt, question) => {
+    const li = document.createElement('li');
+    li.textContent = `${prompt} ${question}`;
+    history.append(li);
+}
+
+function google(e, url, prompt) {
     e.preventDefault();
-    window.open(`https://www.google.com/search?q=Hint without solution: \n${hintInput.value}`);
+    const question = hintInput.value;
+    window.open(`${url}${prompt} \n${question}`);
+    addToHistory(prompt, question);
     hintInput.value = '';
 }
+
+[hintForm].forEach(el => el.addEventListener('submit', (e) => {
+    google(e, googleUrl, hintLabel.textContent);
+}));
 
