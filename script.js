98c0fa53a6199505509565cd1c165c4fdb397c7a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-15 11:43:59 +0200

    fix(google func): remove new line between prompt and querstion

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-// // const hintForm = document.getElementById('hint-form');
-// const hintInput = document.getElementById('hint-input');
-// const hintLabel = document.getElementById('hint-label');
-
-// const safeForm = document.getElementById('safe-form');
-// const safeInput = document.getElementById('safe-input');
-// const safeLabel = document.getElementById('safe-label');
-
 const history = document.getElementById('history');
 
 const googleUrl = `https://www.google.com/search?q=`;
 
-const addToHistory = (prompt, question) => {
-    const li = document.createElement('li');
-    li.textContent = `${prompt} ${question}`;
-    history.append(li);
-}
 
 function google(url, question, prompt, e) {
     e.preventDefault();
     try {
-        window.open(`${url}${prompt} \n${question}`);
+        window.open(`${url}${prompt}${question}`);
     } catch (error) {
         alert('Not open');
     }
+    debugger
     addToHistory(prompt, question);
     // hintInput.value = ''; // not working
 }
-// const copyInput = inputText => { }
-
-// const labels = [hintLabel, safeLabel];
-// const inputs = [hintInput, safeInput];
 
 class Section {
     constructor(name) {
@@ -37,9 +21,24 @@ class Section {
         this.input = document.getElementById(`${name}-input`);
         this.label = document.getElementById(`${name}-label`);
     }
+
+    // google() {}
+
+    addToHistory() {
+
+    }
+
+
+}
+const addToHistory = (prompt, question) => {
+    const li = document.createElement('li');
+    li.textContent = `${prompt} ${question}`;
+    history.append(li);
 }
 
 
 const hintObj = new Section('hint');
 const safeObj = new Section('safe');
-[hintObj, safeObj].forEach(obj => obj.form.addEventListener('submit', (e) => google(googleUrl, obj.input.value, obj.label.textContent, e)));
+const generalObj = new Section('general');
+
+[hintObj, safeObj, generalObj].forEach(obj => obj.form.addEventListener('submit', (e) => google(googleUrl, obj.input.value, obj.label.textContent, e)));
